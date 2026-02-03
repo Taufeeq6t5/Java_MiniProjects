c75a2fc787c204075eff1e7a09ce3ec7033f9dcb
Taufeeq6t5 <taufeeqahmed.iet@srinivasuniversity.edu.in>
2026-02-03 18:46:50 +0530

    Added Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Calculator{
+    public static void main (String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("");
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
