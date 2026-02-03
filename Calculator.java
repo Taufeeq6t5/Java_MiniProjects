6259d2e4dba9fd51bfd74155c0720e559b4ffa22
Taufeeq6t5 <taufeeqahmed.iet@srinivasuniversity.edu.in>
2026-02-03 18:59:09 +0530

    Updated Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -3,7 +3,81 @@ import java.util.Scanner;
 public class Calculator{
     public static void main (String args[]){
         Scanner sc=new Scanner(System.in);
-        System.out.println("");
+        int choice;
+        double result=0;
+        while(1){
+        System.out.println("\n******Calculator Menu******");
+        System.out.println("1.Addition.");
+        System.out.println("2.Subtraction");
+        System.out.println("3.Multiplication");
+        System.out.println("4.Division");
+        System.out.println("5.Modulus");
+        System.out.println("6.Power");
+        System.out.println("7.Square");
+        System.out.println("8.Exit");
+
+        System.out.println("Enter your choice:");
+        choice =sc.nextInt();
+
+        if(choice>=1 && choice<=6 ){
+            System.out.print("Enter the first number:");
+            int a=sc.nextInt();
+            System.out.println("Enter the second number:");
+            int b=sc.nextInt();
+
+            switch(choice){
+                case 1:result=a+b;
+                System.out.println("Result= "+result);
+                break;
+
+                case 2:result=a-b;
+                System.out.println("Result= "+result);
+                break;
+
+                case 3:result=a*b;
+                System.out.println("Result= "+result);
+                break;
+
+                case 4:if(b==0)
+                System.out.println("Error Division By Zero..!");
+                else
+                result=a/b;
+                System.out.println("Result= "+result);
+                break;
+
+                case 5:if(b==0)
+                System.out.println("Error Modulus By Zero..!");
+                else
+                result=a%b;
+                System.out.println("Result= "+result);
+                break;
+
+                case 6:result=math.pow(a,b);
+                System.out.println("Result= "+result);
+                break;
+
+            }
+
+        }
+
+        else if(choice==7){
+            System.out.println("Enter the number:");
+            int num=sc.nextInt();
+
+            result=num*num;
+            System.out.println("Result= "+result);
+
+        }
+        else if (choice==8){
+            System.out.println("Exiting Calculator..");
+        }
+        else{
+            System.out.println("Invalid Choice!");
+        }
+
+
+
+        }
 
 
 
