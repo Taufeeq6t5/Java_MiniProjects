7702c53095e3e4275e6627892335e6ee9a8492d0
Taufeeq Ahmed <taufeeqahmed.iet@srinivasuniversity.edu.in>
2026-03-16 08:20:23 +0530

    Added ATMService.java
    simple Java ATM simulation program that allows users to perform balance enquiry, deposit, and withdraw operations using a menu-driven interface.

--- ATMService.java
+++ ATMService.java
@@ -1,1 +1,56 @@
+import java.util.Scanner;
 
+public class ATMService {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int choice;
+        double balance = 10000;
+
+        do {
+            System.out.println("\n--- ATM MENU ---");
+            System.out.println("1. Balance Enquiry");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Exit");
+            System.out.print("Enter your choice: ");
+
+            choice = sc.nextInt();
+
+            switch (choice) {
+
+                case 1:
+                    System.out.println("Current Balance: " + balance);
+                    break;
+
+                case 2:
+                    System.out.print("Enter amount to deposit: ");
+                    double deposit = sc.nextDouble();
+                    balance = balance + deposit;
+                    System.out.println("Amount Deposited Successfully");
+                    break;
+
+                case 3:
+                    System.out.print("Enter amount to withdraw: ");
+                    double withdraw = sc.nextDouble();
+                    if (withdraw <= balance) {
+                        balance = balance - withdraw;
+                        System.out.println("Please collect your cash");
+                    } else {
+                        System.out.println("Insufficient Balance");
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Thank you for using ATM");
+                    break;
+
+                default:
+                    System.out.println("Invalid Choice");
+            }
+
+        } while (choice != 4);
+
+        sc.close();
+    }
+}
